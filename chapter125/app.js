b31a0d307df48abfebf151daa00351e1e79f47c7
adnanmk4 <135832600+adnanmk4@users.noreply.github.com>
2023-08-09 07:22:07 -0700

    chapter125

---
+++ chapter125/app.js
@@ -0,0 +1,49 @@
+// // -------------chapter1---------
+
+// alert("message");
+
+// // -------------chapter2---------
+
+// var fullname = ("Adnanbaloch")
+// alert(fullname)
+// var email = ("example@gmail.com")
+// alert(email)
+// var book = "A smarter way to learn JavaScript";
+// alert(book);
+
+// // -------------chapter3---------
+
+// var age = 25;
+// alert("My age is " + age);
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+
+// var message = visitorName + " ordered " + quantity + "" + productTitle + "(s) on XYZ Clothing store";
+
+// alert(message);
+
+// // -------------chapter4---------
+
+// var variableOne, variableTwo, variableThree;
+// var variable1, _variable, $variable, variable$, variable_n;
+
+// // -------------chapter5---------
+
+// // var num;
+
+// // document.write("Value after variable declaration is: " + num + "<br/>");
+// // num = 5;
+
+// // document.write("Initial value: " + num + "<br/>");
+// // num++;
+
+// // document.write("Value after increment is: " + num + "<br/>");
+// // num += 6;
+
+// // document.write("Value after addition is: " + num + "<br/>");
+
+// // var remainder = num % 3;
+
+// // document.write("The remainder is: " + remainder);
+
